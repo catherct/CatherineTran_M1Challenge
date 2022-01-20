e5637abdac2ebbd70e750665c979a89177c00ebe
Catherine Tran <cathy_tran@Catherines-MacBook-Air-2.local>
2022-01-19 23:05:45 -0800

    Reflecting on goals for Java Foundations

---
+++ CourseGoals/src/main/java/com/company/Goals.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Goals {
+
+    // type psvm (public static void main) tab to produce the following line
+    public static void main(String[] args) {
+
+        System.out.println("Challenge myself to develop a web application that I'm proud of.");
+
+        System.out.println("Acquire and sharpen the skills I need to be a confident and qualified job applicant.");
+
+        System.out.format("Make at least %d connections from the program, and at least %f from those connections.", 5, 1.5);
+
+    }
+}
